eb0b3ab607ac8ee13dc7b7835daa0db1aef6b818
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-16 03:07:36 +0500

    first commit

--- api/main.go
+++ api/main.go
@@ -10,11 +10,20 @@ import (
 	"github.com/rs/cors"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var mongoURI = "mongodb+srv://Abdullah1:Abdullah1@cluster0.agxpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 var client *mongo.Client
 var usersCollection *mongo.Collection
+
+type User struct {
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	Gender      string `json:"gender"`
+	CompanyName string `json:"company_name"`
+}
 
 // init function to set up MongoDB connection
 func init() {
@@ -30,6 +39,40 @@ func initMongo() {
 	usersCollection = client.Database("test").Collection("users")
 }
 
+func sigup(w http.ResponseWriter, r *http.Request) {
+
+	var user User
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var existingUser User
+	err := usersCollection.FindOne(context.TODO(), map[string]string{"email": user.Email}).Decode(&existingUser)
+	if err == nil {
+		http.Error(w, "Email already exists", http.StatusConflict)
+		return
+	}
+	hasshedpassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+	user.Password = string(hasshedpassword)
+	_, err = usersCollection.InsertOne(context.TODO(), user)
+
+	if err != nil {
+		http.Error(w, "Error creating user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+	return
+}
+
 // Handler function to handle incoming requests
 func Handler(w http.ResponseWriter, r *http.Request) {
 	router := mux.NewRouter()
@@ -41,7 +84,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			"message": "hello world",
 		})
 	}).Methods("GET")
-
+	router.HandleFunc("/signup", sigup).Methods("POST")
 	// CORS handler setup
 	corsHandler := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
@@ -52,5 +95,3 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// Serve HTTP with CORS enabled
 	corsHandler.ServeHTTP(w, r)
 }
-
-// main function to start the server
