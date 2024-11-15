eedc96c272d7dc74da78c87394d57dc3dc67db15
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-16 02:29:46 +0500

    first commit

--- api/main.go
+++ api/main.go
@@ -1,6 +1,4 @@
-
 package main
-
 
 import (
 	"context"
@@ -13,42 +11,49 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongodburl = "mongodb+srv://Abdullah1:Abdullah1@cluster0.agxpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://Abdullah1:Abdullah1@cluster0.agxpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+var mongodburl = "mongodb+srv://Abdullah1:Abdullah1@cluster0.agxpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 var client *mongo.Client
+var usercollection *mongo.Collection
 
-var usercollection *mongo.Collection;
-
-func init(){
+// init function to set up MongoDB connection
+func init() {
 	initMongo()
 }
 
-func initMongo(){
-	var err error;
-	client,err =mongo.Connect(context.TODO(),options.Client().ApplyURI(mongodburl))
-      if err !=nil{
-		log.Fatal("error connection mongodb",err)
-	  }
-    usercollection=client.Database("test").Collection("userdata")
+// Function to initialize MongoDB connection
+func initMongo() {
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongodburl))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+	usercollection = client.Database("test").Collection("userdata")
 }
 
+// Handler function to handle incoming requests
 func handler(w http.ResponseWriter, r *http.Request) {
-	router :=mux.NewRouter();
-	router.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	// Route to handle a simple GET request
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{
 			"message": "hello go from vercel!!!",
 		})
 	}).Methods("GET")
 
-
-	corsHandler:=cors.New(cors.Options{
+	// CORS handler setup
+	corsHandler := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
-        AllowedMethods: []string{"GET","POST","PUT","DELETE"},
-        AllowedHeaders: []string{"Authorization","Content-Type"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Authorization", "Content-Type"},
 	}).Handler(router)
-	corsHandler.ServeHTTP(w,r)
+
+	// Serve HTTP with CORS enabled
+	corsHandler.ServeHTTP(w, r)
 }
 
+// main function to start the server
 func main() {
 	// Define the endpoint and the handler
 	http.HandleFunc("/", handler)
