1b2288d10f05f41c605d39ae19870cfb3da8ece2
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-16 03:13:35 +0500

    first commit

--- api/main.go
+++ api/main.go
@@ -70,7 +70,7 @@ func sigup(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(user)
-	return
+	
 }
 
 // Handler function to handle incoming requests
