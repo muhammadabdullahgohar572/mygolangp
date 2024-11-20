eb66b4d5979d750daa35cfb3e90d87cab5663e13
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-20 21:05:00 +0500

    first commit

--- api/main.go
+++ api/main.go
@@ -1,126 +1,67 @@
 package handler
 
 import (
-    "bytes"
-    "context"
-    "encoding/json"
-    "io"
-    "log"
-    "net/http"
-    "time"
-    "github.com/golang-jwt/jwt/v4"
-    "github.com/gorilla/mux"
-    "github.com/rs/cors"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "golang.org/x/crypto/bcrypt"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"golang.org/x/crypto/bcrypt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// JWT secret key
 var jwtSecret = []byte("abdullah55")
-
-// MongoDB URI and collections
 var mongoURI = "mongodb+srv://Abdullah1:Abdullah1@cluster0.agxpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// User and Login structs
+type User struct {
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	Gender      string `json:"gender"`
+	CompanyName string `json:"company_name"`
+}
+
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
 var client *mongo.Client
 var usersCollection *mongo.Collection
 
-// Struct for login request
-type Login struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+func init() {
+	initMongo()
 }
 
-// Struct for user data
-type User struct {
-    Username    string `json:"username"`
-    Password    string `json:"password"`
-    Email       string `json:"email"`
-    Gender      string `json:"gender"`
-    CompanyName string `json:"companyname"`
-}
-
-// Struct for JWT claims
-type Claims struct {
-    Email string `json:"email"`
-    jwt.StandardClaims
-}
-
-// Initialize MongoDB connection
 func initMongo() {
-    var err error
-    client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
-    if err != nil {
-        log.Fatal("Error connecting to MongoDB:", err)
-    }
-    usersCollection = client.Database("test").Collection("users")
-    log.Println("MongoDB connection established")
-}
-// Login handler
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	var loginData Login
-	body, err := io.ReadAll(r.Body)
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
+		log.Fatal("Error connecting to MongoDB:", err)
 	}
-	log.Println("Login request body:", string(body))
-	r.Body = io.NopCloser(bytes.NewBuffer(body)) // Reset r.Body for decoding
-
-	if err := json.NewDecoder(r.Body).Decode(&loginData); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-
-	var existingUser User
-	err = usersCollection.FindOne(context.TODO(), bson.M{"email": loginData.Email}).Decode(&existingUser)
-	if err != nil {
-		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(loginData.Password))
-	if err != nil {
-		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-		return
-	}
-
-	claims := &Claims{
-		Email: loginData.Email,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(20 * time.Hour).Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtSecret)
-	if err != nil {
-		http.Error(w, "Could not create token", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+	usersCollection = client.Database("test").Collection("users")
 }
 
-// Signup handler
+// Signup, login, and protected route handler functions
 func signupHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-	log.Println("Signup request body:", string(body))
-	r.Body = io.NopCloser(bytes.NewBuffer(body)) // Reset r.Body for decoding
-
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	var existingUser User
-	err = usersCollection.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&existingUser)
+	err := usersCollection.FindOne(context.TODO(), map[string]string{"email": user.Email}).Decode(&existingUser)
 	if err == nil {
 		http.Error(w, "Email already exists", http.StatusConflict)
 		return
@@ -140,16 +81,45 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"status":  "success",
-		"message": "User created successfully",
-		"data": map[string]interface{}{
-			"username":    user.Username,
-			"email":       user.Email,
-			"companyname": user.CompanyName,
-			"gender":      user.Gender,
+	json.NewEncoder(w).Encode("User created")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var loginData Login
+	if err := json.NewDecoder(r.Body).Decode(&loginData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var existingUser User
+	err := usersCollection.FindOne(context.TODO(), map[string]string{"email": loginData.Email}).Decode(&existingUser)
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(loginData.Password))
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Email: loginData.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
 		},
-	})
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		http.Error(w, "Could not create token", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
 }
 
 // Main function
